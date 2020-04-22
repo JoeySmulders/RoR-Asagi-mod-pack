fa8f6e801116ecbc199997fc760eb524411e0771
Joey Smulders <joey_smulders@msn.com>
2020-04-22 20:54:30 +0200

    Scythe is now fully synced, scythe also does a small aoe now

--- Items/big_scythe.lua
+++ Items/big_scythe.lua
@@ -7,47 +7,42 @@ item.pickupText = "Deal massive damage to a random enemy"
 item.sprite = Sprite.load("Items/sprites/Big_scythe", 1, 13, 13)
 item:setTier("use")
 item.isUseItem = true
-item.useCooldown = 1 -- 30
+item.useCooldown = 30
 
 ItemPool.find("enigma", "vanilla"):add(item)
 
 -- TODO: make it prioritize enemies on screen first
-
+-- TODO: add a visual effect for it
 
 scytheAttack = net.Packet("Big Scythe Packet", function(player, enemyX, enemyY)
-    player:fireExplosion(enemyX, enemyY, 5, 5, 1000, nil, nil, DAMAGER_NO_PROC)
-    log(player)
-
-    if net.host then
-        --scytheAttack:sendAsHost(net.EXCLUDE, player, enemyX, enemyY)
-    end
+    player:fireExplosion(enemyX, enemyY, 5 / 19, 5 / 4, 1000, nil, nil, DAMAGER_NO_PROC)
 end)
 
 item:addCallback("use", function(player, embryo)
-	local count = 1
-	-- Hit two enemies if embryo is procced
-	if embryo then
-		count = 2
-    end
+    if not net.online or net.localPlayer == player then
+        local count = 1
+        -- Hit two enemies if embryo is procced
+        if embryo then
+            count = 2
+        end
 
-    -- Get the current enemies and take a random one to attack
-    for i = 1, count, 1 do
-        local currentEnemies = enemies:findAll()
-        
-        if currentEnemies then
-            local enemyCount = 0
+        -- Get the current enemies and take a random one to attack
+        for i = 1, count, 1 do
+            local currentEnemies = enemies:findAll()
+            
+            if currentEnemies then
+                local enemyCount = 0
 
-            for i, enemy in ipairs(currentEnemies) do
-                enemyCount = enemyCount + 1
-            end
+                for i, enemy in ipairs(currentEnemies) do
+                    enemyCount = enemyCount + 1
+                end
 
-            local enemyTarget = math.random(1, enemyCount)
+                local enemyTarget = math.random(1, enemyCount)
 
-            for i, enemy in ipairs(currentEnemies) do
-                if i == enemyTarget then
-                    player:fireExplosion(enemy.x, enemy.y, 5, 5, 1000, nil, nil, DAMAGER_NO_PROC)
+                for i, enemy in ipairs(currentEnemies) do
+                    if i == enemyTarget then
+                        player:fireExplosion(enemy.x, enemy.y, 5 / 19, 5 / 4, 1000, nil, nil, DAMAGER_NO_PROC)
 
-                    if net.online then
                         if net.host then
                             scytheAttack:sendAsHost(net.ALL, nil, enemy.x, enemy.x)
                         else
@@ -57,9 +52,7 @@ item:addCallback("use", function(player, embryo)
                 end
             end
         end
-
     end
-    
 
 end)
 
